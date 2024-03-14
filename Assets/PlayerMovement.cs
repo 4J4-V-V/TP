bb94fa3b92ea9fe200198b366d3a604c5740e6f9
mathcdu <mathieucdufour@gmail.com>
2024-03-13 23:22:23 -0400

    SmootherJump + Dash + DoubleJump LOGIC TESTING
    more to come

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,56 +4,121 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    private Rigidbody2D rb;     //r�f�rence au rigidbody 
-    private BoxCollider2D boxCollider; //r�f�rence au boxCollider
-    [SerializeField] private LayerMask JumpableGround;  //r�f�rence au layer du sol
-    [SerializeField] private float jumpForce = 14f;     //force du saut
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask JumpableGround;
     [SerializeField] private float moveSpeed = 7f;
-    private bool canDoubleJump = false;
-     private bool hasPickedUpItem = false; 
+    [SerializeField] private float jumpForce = 14f;
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashSpeed = 24f;
+    private float dashTime = 0.2f;
+    private float dashCooldown = 1f;
+    private bool hasPickedUpItem = false;
+    [SerializeField] private float jumpTime;
+    private float jumpTimeCounter;
+    private bool isJumping;
 
-    // Start is called before the first frame update
+    private int doubleJump;
+    [SerializeField] private int doubleJumpV;
+    [SerializeField] private int doubleJumpF;
+
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();    //r�cup�ration du rigidbody
-        boxCollider = GetComponent<BoxCollider2D>();  //r�cup�ration du boxColliderß
-        StartCoroutine(SimulateItemPickup());
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-        IEnumerator SimulateItemPickup()
-    {
-        yield return new WaitForSeconds(5f); // Wait for 5 seconds
 
-        // Simulate item pickup
-        PickupItem();
-    }
-    // Update is called once per frame
     void Update()
     {
-        float directionX = Input.GetAxisRaw("Horizontal");  //r�cup�ration de la direction en X
-        rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);  //d�placement du joueur
+        if (isDashing)
+        {
+            return;
+        }
 
-            if (Input.GetButtonDown("Jump"))
-                if(IsGrounded())
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                    canDoubleJump = true; // Reset double jump ability when grounded
-                }  //si on appuie sur la touche de saut et que le joueur est au sol
-                else if (canDoubleJump && hasPickedUpItem) // Check if double jump is allowed and the item has been picked up
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                    canDoubleJump = false; // Prevent further double jumps until grounded
-                }
+        float directionX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);
+
+        if (directionX != 0)
+        {
+            transform.localScale = new Vector3(directionX, 1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter = jumpTime;
+            isJumping = true;
+        }
+        else if (hasPickedUpItem && !IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (Input.GetButton("Jump") && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+
+        if (Input.GetButtonDown("Dash") && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+        extraJump();
     }
-    private bool IsGrounded(){
-        // Create a box around the player that has the same shape of the box collider Second box over it , 0 is just rotation , vector 2. down - move the box down Checks if we are overlapping this jumpable ground
-        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, JumpableGround);
+
+    private bool IsGrounded()
+    {
+        bool grounded = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, JumpableGround);
+        return grounded;
     }
-        public void PickupItem() // ****** Need to call this somwhere when the player picks up the item
+
+    public void PickupItem()
     {
         hasPickedUpItem = true;
     }
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0);
+        yield return new WaitForSeconds(dashTime);
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+void extraJump()
+{
+    if (Input.GetButtonDown("Jump") && doubleJump > 0 && !IsGrounded())
+    {
+        rb.velocity = Vector2.up * doubleJumpF;
+        doubleJump--; // Decrement double jump counter only for double jumps
+    } 
+    else if (Input.GetButtonDown("Jump") && IsGrounded())
+    {
+        rb.velocity = Vector2.up * jumpForce;
+        doubleJump = doubleJumpV; // Reset double jump counter when grounded
+    }
+}
 }
 
 
